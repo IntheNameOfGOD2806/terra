07d1fc7c67fff62d107a31a4457d6e000f206aa3
Trần thành Đạt <101255344+IntheNameOfGOD2806@users.noreply.github.com>
2026-01-17 11:56:27 +0700

    cxvx
    vxv

--- main.tf
+++ main.tf
@@ -11,20 +11,23 @@ resource "aws_instance" "k8s_master" {
 
   # 
   connection {
-    type = "ssh"
-    user = "ubuntu"
+    type        = "ssh"
+    user        = "ubuntu"
     private_key = file("k8s")
-    host = self.public_ip
+    host        = self.public_ip
   }
   provisioner "file" {
-    source = "./master.sh"
+    source      = "./master.sh"
     destination = "/home/ubuntu/master.sh"
   }
   provisioner "remote-exec" {
-    inline = [ 
+    inline = [
       "chmod +x /home/ubuntu/master.sh",
       "sudo sh /home/ubuntu/master.sh k8s-master"
-     ]
+    ]
+  }
+  provisioner "local-exec" {
+    command = "ansible-playbook -i '${self.public_ip},' playbook.yml"
   }
 }
 
@@ -34,30 +37,37 @@ resource "aws_instance" "k8s_master" {
 #launch worker node
 
 resource "aws_instance" "k8s_worker" {
-  count = var.worker_count
+  count         = var.worker_count
   ami           = var.ami["worker"]
   instance_type = var.instance_type["worker"]
   tags = {
     Name = "k8s_worker-${count.index}"
   }
   key_name        = aws_key_pair.k8s.key_name
-  security_groups = [aws_security_group.k8s_master.id]
+  security_groups = [aws_security_group.k8s_worker.id]
+  depends_on      = [aws_instance.k8s_master]
 
   # 
   connection {
-    type = "ssh"
-    user = "ubuntu"
+    type        = "ssh"
+    user        = "ubuntu"
     private_key = file("k8s")
-    host = self.public_ip
+    host        = self.public_ip
   }
   provisioner "file" {
-    source = "./worker.sh"
+    source      = "./worker.sh"
     destination = "/home/ubuntu/worker.sh"
   }
+  provisioner "file" {
+    source      = "./join-command.sh"
+    destination = "/home/ubuntu/join-command.sh"
+  }
   provisioner "remote-exec" {
-    inline = [ 
+    inline = [
       "chmod +x /home/ubuntu/worker.sh",
-      "sudo sh /home/ubuntu/worker.sh k8s-worker-${count.index}"
-     ]
+      "sudo sh /home/ubuntu/worker.sh k8s-worker-${count.index}",
+      "chmod +x /home/ubuntu/join-command.sh",
+      "sudo sh /home/ubuntu/join-command.sh"
+    ]
   }
 }
