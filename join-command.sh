79fe6b6da85bea718410d60869f3112a72200a81
Trần thành Đạt <101255344+IntheNameOfGOD2806@users.noreply.github.com>
2026-01-29 09:40:36 +0700

    fdgd

--- join-command.sh
+++ join-command.sh
@@ -1,1 +1,1 @@
-kubeadm join 10.0.1.81:6443 --token qi6xcd.mrz1i3i47t7uy186 --discovery-token-ca-cert-hash sha256:20d8618fb929ddd1e3545b2eb2e220a6b411b325de51b2c7a443fe773a2d9241 
+kubeadm join 10.0.1.232:6443 --token qijkyj.7528knnqa3qhdmel --discovery-token-ca-cert-hash sha256:144e0b64e3c2ccd527229a4e1339c1e018ec82752a918b17a9f4f2af85f62d84 
--- security_groups.tf
+++ security_groups.tf
@@ -12,97 +12,113 @@ resource "aws_vpc_security_group_ingress_rule" "SSH" {
   security_group_id = aws_security_group.k8s_master.id
   cidr_ipv4         = "0.0.0.0/0"
   #cidr_ipv6         = "::/0"
-  from_port         = 22
-  ip_protocol       = "tcp"
-  to_port           = 22
+  from_port   = 22
+  ip_protocol = "tcp"
+  to_port     = 22
 }
 
 resource "aws_vpc_security_group_ingress_rule" "api_server" {
   security_group_id = aws_security_group.k8s_master.id
   cidr_ipv4         = "0.0.0.0/0"
   #cidr_ipv6         = "::/0"
-  from_port         = 6443
-  ip_protocol       = "tcp"
-  to_port           = 6443
+  from_port   = 6443
+  ip_protocol = "tcp"
+  to_port     = 6443
 }
 
 resource "aws_vpc_security_group_ingress_rule" "ETCD" {
   security_group_id = aws_security_group.k8s_master.id
   cidr_ipv4         = "0.0.0.0/0"
   #cidr_ipv6         = "::/0"
-  from_port         = 2379
-  ip_protocol       = "tcp"
-  to_port           = 2380
+  from_port   = 2379
+  ip_protocol = "tcp"
+  to_port     = 2380
 }
 
 resource "aws_vpc_security_group_ingress_rule" "weavenet_tcp" {
   security_group_id = aws_security_group.k8s_master.id
   cidr_ipv4         = "0.0.0.0/0"
   #cidr_ipv6         = "::/0"
-  from_port         = 6783
-  ip_protocol       = "tcp"
-  to_port           = 6783
+  from_port   = 6783
+  ip_protocol = "tcp"
+  to_port     = 6783
+}
+# resource "aws_vpc_security_group_ingress_rule" "rancher_444" {
+#   security_group_id = aws_security_group.k8s_master.id
+#   cidr_ipv4         = "0.0.0.0/0"
+#   #cidr_ipv6         = "::/0"
+#   from_port   = 444
+#   ip_protocol = "tcp"
+#   to_port     = 444
+# }
+resource "aws_vpc_security_group_ingress_rule" "rancher_81" {
+  security_group_id = aws_security_group.k8s_master.id
+  cidr_ipv4         = "0.0.0.0/0"
+  #cidr_ipv6         = "::/0"
+  from_port   = 81
+  ip_protocol = "tcp"
+  to_port     = 81
 }
 
 resource "aws_vpc_security_group_ingress_rule" "weavenet_udp" {
   security_group_id = aws_security_group.k8s_master.id
   cidr_ipv4         = "0.0.0.0/0"
   #cidr_ipv6         = "::/0"
-  from_port         = 6784
-  ip_protocol       = "udp"
-  to_port           = 6784
+  from_port   = 6784
+  ip_protocol = "udp"
+  to_port     = 6784
 }
 
 resource "aws_vpc_security_group_ingress_rule" "kubelet_and_control_plane" {
   security_group_id = aws_security_group.k8s_master.id
   cidr_ipv4         = "0.0.0.0/0"
   #cidr_ipv6         = "::/0"
-  from_port         = 10248
-  ip_protocol       = "tcp"
-  to_port           = 10260
+  from_port   = 10248
+  ip_protocol = "tcp"
+  to_port     = 10260
 }
 
 resource "aws_vpc_security_group_ingress_rule" "NodePort_Service" {
   security_group_id = aws_security_group.k8s_master.id
   cidr_ipv4         = "0.0.0.0/0"
   #cidr_ipv6         = "::/0"
-  from_port         = 30000
-  ip_protocol       = "tcp"
-  to_port           = 32767
+  from_port   = 30000
+  ip_protocol = "tcp"
+  to_port     = 32767
 }
 
 resource "aws_vpc_security_group_ingress_rule" "HTTP" {
   security_group_id = aws_security_group.k8s_master.id
   cidr_ipv4         = "0.0.0.0/0"
   #cidr_ipv6         = "::/0"
-  from_port         = 80
-  ip_protocol       = "tcp"
-  to_port           = 80
+  from_port   = 80
+  ip_protocol = "tcp"
+  to_port     = 80
 }
 
 resource "aws_vpc_security_group_ingress_rule" "HTTPS" {
   security_group_id = aws_security_group.k8s_master.id
   cidr_ipv4         = "0.0.0.0/0"
   #cidr_ipv6         = "::/0"
-  from_port         = 443
-  ip_protocol       = "tcp"
-  to_port           = 443
+  from_port   = 443
+  ip_protocol = "tcp"
+  to_port     = 443
 }
 
 resource "aws_vpc_security_group_ingress_rule" "HTTP_8080" {
   security_group_id = aws_security_group.k8s_master.id
   cidr_ipv4         = "0.0.0.0/0"
   #cidr_ipv6         = "::/0"
-  from_port         = 8080
-  ip_protocol       = "tcp"
-  to_port           = 8080
+  from_port   = 8080
+  ip_protocol = "tcp"
+  to_port     = 8080
 }
 
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic" {
   security_group_id = aws_security_group.k8s_master.id
   cidr_ipv4         = "0.0.0.0/0"
   #cidr_ipv6         = "::/0"
-  ip_protocol       = "-1" # semantically equivalent to all ports
+  ip_protocol = "-1" # semantically equivalent to all ports
 }
 
 
@@ -121,90 +137,90 @@ resource "aws_vpc_security_group_ingress_rule" "SSH_worker" {
   security_group_id = aws_security_group.k8s_worker.id
   cidr_ipv4         = "0.0.0.0/0"
   #cidr_ipv6         = "::/0"
-  from_port         = 22
-  ip_protocol       = "tcp"
-  to_port           = 22
+  from_port   = 22
+  ip_protocol = "tcp"
+  to_port     = 22
 }
 
 resource "aws_vpc_security_group_ingress_rule" "api_server_worker" {
   security_group_id = aws_security_group.k8s_worker.id
   cidr_ipv4         = "0.0.0.0/0"
   #cidr_ipv6         = "::/0"
-  from_port         = 6443
-  ip_protocol       = "tcp"
-  to_port           = 6443
+  from_port   = 6443
+  ip_protocol = "tcp"
+  to_port     = 6443
 }
 
 resource "aws_vpc_security_group_ingress_rule" "ETCD_worker" {
   security_group_id = aws_security_group.k8s_worker.id
   cidr_ipv4         = "0.0.0.0/0"
   #cidr_ipv6         = "::/0"
-  from_port         = 2379
-  ip_protocol       = "tcp"
-  to_port           = 2380
+  from_port   = 2379
+  ip_protocol = "tcp"
+  to_port     = 2380
 }
 
 resource "aws_vpc_security_group_ingress_rule" "weavenet_tcp_worker" {
   security_group_id = aws_security_group.k8s_worker.id
   cidr_ipv4         = "0.0.0.0/0"
   #cidr_ipv6         = "::/0"
-  from_port         = 6783
-  ip_protocol       = "tcp"
-  to_port           = 6783
+  from_port   = 6783
+  ip_protocol = "tcp"
+  to_port     = 6783
 }
 
 resource "aws_vpc_security_group_ingress_rule" "weavenet_udp_worker" {
   security_group_id = aws_security_group.k8s_worker.id
   cidr_ipv4         = "0.0.0.0/0"
   #cidr_ipv6         = "::/0"
-  from_port         = 6784
-  ip_protocol       = "udp"
-  to_port           = 6784
+  from_port   = 6784
+  ip_protocol = "udp"
+  to_port     = 6784
 }
 
 resource "aws_vpc_security_group_ingress_rule" "kubelet_and_control_plane_worker" {
   security_group_id = aws_security_group.k8s_worker.id
   cidr_ipv4         = "0.0.0.0/0"
   #cidr_ipv6         = "::/0"
-  from_port         = 10248
-  ip_protocol       = "tcp"
-  to_port           = 10260
+  from_port   = 10248
+  ip_protocol = "tcp"
+  to_port     = 10260
 }
 
 resource "aws_vpc_security_group_ingress_rule" "NodePort_Service_worker" {
   security_group_id = aws_security_group.k8s_worker.id
   cidr_ipv4         = "0.0.0.0/0"
   #cidr_ipv6         = "::/0"
-  from_port         = 30000
-  ip_protocol       = "tcp"
-  to_port           = 32767
+  from_port   = 30000
+  ip_protocol = "tcp"
+  to_port     = 32767
 }
 
 resource "aws_vpc_security_group_ingress_rule" "HTTP_worker" {
   security_group_id = aws_security_group.k8s_worker.id
   cidr_ipv4         = "0.0.0.0/0"
   #cidr_ipv6         = "::/0"
-  from_port         = 80
-  ip_protocol       = "tcp"
-  to_port           = 80
+  from_port   = 80
+  ip_protocol = "tcp"
+  to_port     = 80
 }
 
 resource "aws_vpc_security_group_ingress_rule" "HTTPS_worker" {
   security_group_id = aws_security_group.k8s_worker.id
   cidr_ipv4         = "0.0.0.0/0"
   #cidr_ipv6         = "::/0"
-  from_port         = 443
-  ip_protocol       = "tcp"
-  to_port           = 443
+  from_port   = 443
+  ip_protocol = "tcp"
+  to_port     = 443
 }
 
 resource "aws_vpc_security_group_ingress_rule" "HTTP_8080_worker" {
   security_group_id = aws_security_group.k8s_worker.id
   cidr_ipv4         = "0.0.0.0/0"
   #cidr_ipv6         = "::/0"
-  from_port         = 8080
-  ip_protocol       = "tcp"
-  to_port           = 8080
+  from_port   = 8080
+  ip_protocol = "tcp"
+  to_port     = 8080
 }
 
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_worker" {
