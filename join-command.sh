3d84e00b74937bf6ae368c88c109cbe274b46d97
Trần thành Đạt <101255344+IntheNameOfGOD2806@users.noreply.github.com>
2026-02-10 17:24:52 +0700

    b

--- join-command.sh
+++ join-command.sh
@@ -1,1 +1,1 @@
-kubeadm join 10.0.1.86:6443 --token qrj4e6.acui2hxdootxahs7 --discovery-token-ca-cert-hash sha256:5b7e4a49d3b69c21abe9f741c1e219e090c194fdffac20316b77a2e8bccfef7e 
+kubeadm join 10.0.1.93:6443 --token uhdiyt.zowgakhkl3o6upi4 --discovery-token-ca-cert-hash sha256:9a6736badebcbbc555bb26da29bea8e8b1f8776bf8f3c72dce4cf0d3438ae1e3 
